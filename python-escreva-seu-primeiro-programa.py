8f2543010a42d7064f544f509abe2ca1de341ed8
Viccenzo de Oliveira N. Resende <128618121+oviccenzo@users.noreply.github.com>
2026-05-25 22:30:51 -0300

    Update python-escreva-seu-primeiro-programa.py

--- python-escreva-seu-primeiro-programa.py
+++ python-escreva-seu-primeiro-programa.py
@@ -12,7 +12,7 @@ while num2 > 0:
   print("O valor real do numero eh: {0}".format(num1 - (num1 * (num2 * 0.01))))
 
 salario1 = int(input("Salario?: "))
-imposto1 = 27. 
+imposto1 = 27.
 while imposto1 > 0:
   imposto1 = (input("Imposto ou (0) para sair: "))
   if not imposto1:
@@ -28,7 +28,7 @@ totalDoSalario = saldo * salario
 
 print(f"O total do saldo com porcentagem é: {totalDoSalario}")
 
-complex(12 + 2, 2 + 20) #n1 + n2 , n2 + n1 
+complex(12 + 2, 2 + 20) #n1 + n2 , n2 + n1
 
 n1 = int(input("Digite o primeiro numero -> n1: "))
 n2 = int(input("Digite o segundo numero -> n2: "))
@@ -43,9 +43,17 @@ print(f"O numero de n3 eh: {n3}")
 print(f"O numero de n4 eh: {n4}")
 
 somaTotal = n1 + n2 + n3 + n4
+multplicacaoTotal = n1 * n2 * n3 * n4
+subtracaoTotal = n1 - n2 - n3 - n4
+divisãoTotal = n1 / n2 / n3 / n4
 
-print(f"O numero complexo é: {complex(n2 + n3+ n1 + n4, n2 + n1 +n3 + n4) }")
+print(f"O numero complexo é: {complex(n2 + n3+ n1 + n4 // 2, n2 * n1 * n3 * n4 // 2)}")
 print(f"A soma total é: {somaTotal}")
+print(f"A multiplicacao total é: {multplicacaoTotal}")
+print(f"A subtracao total é: {subtracaoTotal}")
+print(f"A divisao total é: {divisãoTotal}")
+
+print(10 > 1j.imag)
 
 st = 'ola mundo'
 
@@ -69,11 +77,11 @@ len(st)
 
 # Capitulo 1: Iniciando com python
 
-1.5 primeiros passo abra o interpretador
+####1.5 primeiros passo abra o interpretador
 
 !python --version
 
-1.6 primeira exploração
+####1.6 primeira exploração
 
 1
 
@@ -83,7 +91,7 @@ print("hello world")
 
 # Capitulo 2: aprendendo python na prática números e strings
 
-2.1-numeros
+####2.1-numeros
 
 print(1) #int
 
@@ -93,7 +101,7 @@ print(1.) #float tambem
 
 print(1+2j) #complex
 
-####Gerando numeros por meios das funções embutidos
+#####Gerando numeros por meios das funções embutidos
 
 int(1.0)
 
@@ -111,7 +119,7 @@ float('nan')
 
 complex(1,2)
 
-####ints e longs unificando no python 3 
+#####ints e longs unificando no python 3
 
 Alguns exemplos de números e manipulações simples
 
@@ -131,7 +139,7 @@ print(complex(2,0) + 0 + 1j)
 
 print(2 + 0 + 1j)
 
-####operadores aritmeticas
+#####operadores aritmeticas
 
 print(1 + 2)
 
@@ -149,7 +157,7 @@ print(-3)
 
 print(2 ** 8)
 
-####operadores de bits
+#####operadores de bits
 
 print(1|0)
 
@@ -165,19 +173,19 @@ print(4 >> 2)
 
 print(~4)
 
-####operações misturando tipos diferentes e as regras de coerção
+#####operações misturando tipos diferentes e as regras de coerção
 
 print(100* 1.3) #preço mais 30%
 
 type(1 + 2.0)
 
-type(1+2j)
+print(type(1+2j))
 
 type(1.0+2j)
 
 type(1.0+1.0)
 
-####2.3-Criando e maniupulando texto: strings
+###2.3-Criando e maniupulando texto: strings
 
 #coding: utf-8
 "copa 2014"
@@ -202,7 +210,7 @@ print(("Copa" "2014") == "Copa2014")
 input('Em qual cidade o legado da copa foi relevant'
       'para a população: ')
 
-####len(strings), explicação len e para o tamanho da string
+#####len(strings), explicação len e para o tamanho da string
 
 st = "macarana"
 
@@ -216,7 +224,7 @@ st[:3]
 
 len(st)
 
-####Sequencias string
+#####Sequencias string
 
 print("m" in "macarana")
 
@@ -224,14 +232,14 @@ print("x" not in "macarana")
 
 ("m" +"aracana")
 
-"a" * 3
+("a" * 3)
 
-####imutabilidade: novas strings criadas a partir de outras strings
+#####imutabilidade: novas strings criadas a partir de outras strings
 
 minha_str = "livro python 3"
 print(minha_str[13] + "2")
 
-####copiando e manipulando texto com a string
+#####copiando e manipulando texto com a string
 
 minha_str = "livro python 3"
 minha_str = minha_str[0:13] + "2"
@@ -255,7 +263,7 @@ print(minha_str)
 
 "copa de 2014".replace("2014","2018")
 
-####interpolando a string
+#####interpolando a string
 
 (("%d dias para copa") % (100))
 
@@ -263,15 +271,17 @@ print(minha_str)
 
 "{dia} dias para copa".format(dia=100)
 
-"{:<60}".format("alinhados á esquerda, ocupando 60 posições")
+"{:<<60}".format("alinhados á esquerda, ocupando 60 posições")
 
-"{:>60}".format("alinhados á esquerda, ocupando 60 posições")
+"{:>>60}".format("alinhados á esquerda, ocupando 60 posições")
 
-"{:^60}".format("centralizados á esquerda, ocupando 60")
+"{:^^60}".format("centralizados á esquerda, ocupando 60")
 
-####operações misturados tipos diferentes e as regras de coerção
+#####operações misturados tipos diferentes e as regras de coerção
 
 print(100 * 1.3) #preço mais de 30%
+print(100 * 1.4)
+print(100 * 1.5)
 
 ####explorando as operações por conta propria
 
@@ -290,17 +300,15 @@ salario = 5000
 print("Salario real: {}".format(salario - (salario * imposto)))
 print("Imposto: {}".format(salario * imposto))
 
-3.1.1
-
-imposto = 0.27
-salario = 3000
-print("Valor real: {0}".format(salario - (salario * imposto)))
+imposto1 = 0.27
+salario1 = 3000
+print("Valor real: {0}".format(salario1 - (salario1 * imposto1)))
 
 ####3.2-Pegando dados no terminal
 
-salario = int(input("Digite o salário: "))
-imposto = float(input("Imposto em % (exemplo: 27.5)? "))
-print("Valor real: {0}".format(salario - (salario * (imposto * 0.01))))
+salario2 = int(input("Digite o salário: "))
+imposto2 = float(input("Imposto em % (exemplo: 27.5)? "))
+print("Valor real: {0}".format(salario2 - (salario2 * (imposto2 * 0.01))))
 
 ####3.3-Comparações: maior, menor, igual e outras
 
@@ -316,90 +324,91 @@ print(2 <= 3)
 
 print(1 == 1.0)
 
-# print(10 >1j) possui o erro esse codigo
+#  print(10 >1j) #possui o erro esse codigo
+#  TypeError: '>' not supported between instances of 'int' and 'complex'
 
 ####3.4-Condicionais if else elif e else
 
-salario1 = int(input("Salario? "))
-imposto1 = float(input("Imposto? "))
-if imposto1 == '':
-  imposto1 = 27.5
+salario4 = int(input("Salario? "))
+imposto4 = float(input("Imposto? "))
+if imposto4 == '':
+  imposto4 = 27.5
 else:
-  imposto1 = float(imposto1)
-print("Valor real: {0}".format(salario1 - (salario1 * (imposto1 * 0.01))))
+  imposto4 = float(imposto4)
+print("Valor real: {0}".format(salario4 - (salario4 * (imposto4 * 0.01))))
 
-indentação dos blocos de códigos
+####indentação dos blocos de códigos
 
-imposto2 = float(input("Imposto? "))
-if imposto2 < 10:
+imposto5 = float(input("Imposto? "))
+if imposto5 < 10:
   print("Medio")
-elif imposto2 < 27.5:
+elif imposto5 < 27.5:
   print("Alto")
 else:
   print("Muito alto")
 
-comando if
+####comando if
 
-salario3 = int(input("Salario? "))
-imposto3 = float(input("Imposto em % (exemplo: 27.5)? "))
+salario6 = int(input("Salario? "))
+imposto6 = float(input("Imposto em % (exemplo: 27.5)? "))
 
-if not imposto3:
-  imposto3 = 27.5
+if not imposto6:
+  imposto6 = 27.5
 else:
-  imposto3 = float(imposto3)
+  imposto6 = float(imposto6)
 
-print("Valor real: {0}".format( salario3 * (imposto3 * 0.01)))
+print("Valor real: {0}".format( salario6 * (imposto6 * 0.01)))
 
-Expressão if
+####Expressão if
 
-imposto4 = 0.3
-"alto" if imposto4 > 0.27 else "baixo"
+imposto7 = 0.3
+"alto" if imposto7 > 0.27 else "baixo"
 
-imposto5 = 0.10
-"alto" if imposto5 > 0.27 else "baixo"
+imposto8 = 0.10
+"alto" if imposto8 > 0.27 else "baixo"
 
-valor_imposto = "alto" if imposto > 0.27 else "baixo"
+valor_imposto = "alto" if imposto8 > 0.27 else "baixo"
 print(valor_imposto)
 
 
-3.5 Operações logicos
+####3.5 Operações logicos
 
-imposto6 = float(input("Imposto? "))
-if imposto6 < 10:
+imposto9 = float(input("Imposto? "))
+if imposto9 < 10:
   print("baixo")
-elif imposto6 >= 10. and imposto6 <= 27.:
+elif imposto9 >= 10. and imposto9 <= 27.:
   print("medio")
-elif imposto6 > 27. and imposto6 < 100:
+elif imposto9 > 27. and imposto9 < 100:
   print("alto")
 else:
   print("imposto invalido")
 
 
-3.6 - loops com while
+####3.6 - loops com while
 
-salario7 = float(input("Salario? "))
-imposto7 = 27.
-while imposto7 > 0:
-  imposto7 = (input("Imposto ou (0) para sair: "))
-  if not imposto7:
-    imposto7 = 27.
+salario10 = float(input("Salario? "))
+imposto10 = 27.
+while imposto10 > 0:
+  imposto10 = (input("Imposto ou (0) para sair: "))
+  if not imposto10:
+    imposto10 = 27.
   else:
-    imposto7 = float(imposto7)
-  print("Valor real: {0} ".format(salario7 - (salario7 * (imposto7 * 0.01))))
+    imposto10 = float(imposto10)
+  print("Valor real: {0} ".format(salario10 - (salario10 * (imposto10 * 0.01))))
 
-o loop pode ser interrompido com um comando o break que é quebrar linha
+####o loop pode ser interrompido com um comando o break que é quebrar linha
 
-salario8 = float(input("Salario? ")) #esse linha falar para digitar qualquer valor do salario
-imposto8 = 27 #esse imposto tem o valor de 27
-while imposto8 > 0:
+salario11 = float(input("Salario? ")) #esse linha falar para digitar qualquer valor do salario
+imposto11 = 27 #esse imposto tem o valor de 27
+while imposto11 > 0:
   imposto8 = input("Imposto ou (s) para sair: ")
-  if not imposto8:
+  if not imposto11:
     imposto = 27.
-  elif imposto8 == 's':
+  elif imposto11 == 's':
     break
   else:
-    imposto8 = float(imposto8)
-  print("Valor real: {0} ".format(salario8 - (salario8 * (imposto8 * 0.01))))
+    imposto11 = float(imposto11)
+  print("Valor real: {0} ".format(salario11 - (salario11 * (imposto11 * 0.01))))
 
 
 ####3.7 - primeiro estrutura de dados: listas
@@ -407,45 +416,47 @@ while imposto8 > 0:
 lista = [1,2,3,4,5]
 print(lista)
 
-lista = ["salario","imposto"]
-print(lista)
+lista1 = ["salario","imposto"]
+print(lista1)
 
-lista = [1, "salario"]
-print(lista)
+lista2 = [1, "salario"]
+print(lista2)
 
-lista = [[1,2,3], "salario",10]
-print(lista)
+lista3 = [[1,2,3], "salario",10]
+print(lista3)
 
 ####Sequencia de lista em python ou seja podemos perguntar seu tamanho e acessar elementos por indices ou trechos
 
-lista =["impostos","salarios","altos","baixos"]
-(lista[0] , lista[1] , lista[2] , lista[3])
+lista4 =["impostos","salarios","altos","baixos"]
+(lista4[0] , lista4[1] , lista4[2] , lista4[3])
 
 ####lista são mutaveis
 
-lista = ["impostos","salarios","altos","baixos"]
-(lista[0] ,lista[1] ,lista[2], lista[3])
+lista5 = ["impostos","salarios","altos","baixos"]
+(lista5[0] ,lista5[1] ,lista5[2], lista5[3])
 
 ####ifs e listas
 
-lista = []
+# lista6 = ['ola',12**2]
+lista6 =[]
 
-if lista:
+print(lista6)
+
+if lista6:
   print("Nunca sou executado")
 else:
   print("Sempre sou executado")
 
 ####3.8 loop pythônicos com for e listas
 
-impostos9 = ["MEI","Simples"]
-for imposto in impostos9:
-  print(impostos9)
+imposto12 = ["MEI","Simples"]
+for imposto in imposto12:
+  print(imposto12)
 
 ####comando for em detalhe
 
-lista = [0,1,2,3,4,5,6,7,8,9,10]
-
-for i in lista:
+lista7 = [0,1,2,3,4,5,6,7,8,9,10]
+for i in lista7:
   print(i)
 
 ####3.9 - Percorrendo intervalos de zero ate n com range()
@@ -453,7 +464,7 @@ for i in lista:
 for i in range(11):
   print(i)
 
-print(range(11-1))
+print(range(11,1))
 
 ####Exemplo do tipo que é range
 
@@ -480,9 +491,11 @@ num1 = int(input("Digite o primeiro numero: "))
 num2  = int(input("Digite o segundo numero: "))
 num3  = int(input("Digite o terceiro numero: "))
 
-soma = ((num1 + num2) * num3)
+soma1 = ((num1 + num2) + num3)
+subtracao1 = ((num1 - num2) - num3)
+multiplicao1 = ((num1 * num2) * num3)
 
-print(range(soma))
+print(range(soma1,subtracao1,subtracao1))
 
 ####3.10 Enumerando coleções com for e função enumerate
 
@@ -491,7 +504,7 @@ impostos = ['MEI - micro empreendedor individual','ICMS - Imposto sobre Operaç�
 for imposto in enumerate(impostos):
   print(imposto)
 
-# salario = [1234,2032,3421,4567,5890]
+5# salario = [1234,2032,3421,4567,5890]
 a = int(input("Digite o primeiro numero a: ")) #44 + 46 = 90 / 2 = 45
 b = int(input("Digite o segundo numero b: "))
 c = int(input("Digite o terceiro numero c: "))
@@ -511,21 +524,21 @@ print(c1)
 
 ####3.12 valores padronizados de argumentos
 
-def salarioDescontadoImposto1(salario9, imposto9 = 27.):
-  return salario9 - (salario9 * (imposto9 * 0.01))
+def salarioDescontadoImposto1(salario13, imposto13 = 27.):
+  return salario13 - (salario13 * (imposto13 * 0.01))
 
 salarioDescontadoImposto1(5000)
 
 ####3.13 parametros nomeados
 
-print(salarioDescontadoImposto1(1000, imposto9 = 0.10))
-print(salarioDescontadoImposto1(2000, imposto9 = 0.10))
-print(salarioDescontadoImposto1(3000, imposto9 = 0.10))
-print(salarioDescontadoImposto1(4000, imposto9 = 0.10))
-print(salarioDescontadoImposto1(5000, imposto9 = 0.10))
-print(salarioDescontadoImposto1(6000, imposto9 = 0.10))
+print(salarioDescontadoImposto1(1000, imposto13 = 0.10))
+print(salarioDescontadoImposto1(2000, imposto13 = 0.10))
+print(salarioDescontadoImposto1(3000, imposto13 = 0.10))
+print(salarioDescontadoImposto1(4000, imposto13 = 0.10))
+print(salarioDescontadoImposto1(5000, imposto13 = 0.10))
+print(salarioDescontadoImposto1(6000, imposto13 = 0.10))
 
-###3.14 recebendo um numero arbitrario de argumentos: packing & unpacking
+####3.14 recebendo um numero arbitrario de argumentos: packing & unpacking
 
 from datetime import date
 
@@ -579,7 +592,7 @@ def unpackingExperiment(**kwargs):
 
 unpackingExperiment(named="Teste", other = "Other")
 
-####3.15 usando código já pronto: importando módulos
+#####3.15 usando código já pronto: importando módulos
 
 import math
 print(math.sqrt(9))
@@ -617,7 +630,7 @@ print(l2(1024))
 
 # Capitulo 4: primeiro programa: download de dados da copa 2014
 
-4.1 criando uma função para fazer dowloada no navegador da web e ler o tamanho do arquivos
+####4.1 criando uma função para fazer dowloada no navegador da web e ler o tamanho do arquivos
 
 ##indereço do buffe size
 BUFF_SIZE = 1024
@@ -630,7 +643,7 @@ def dowloadLength(response, output, length):
     print("Dowloaded %d " % (((time * BUFF_SIZE)/length)*100))
 
 
-criando outra função para download na web
+#####criando outra função para download na web
 
 def dowload(response, output):
   totalDowloaded = 0
@@ -643,7 +656,7 @@ def dowload(response, output):
     print(f"Downloaded {bytes}".format(bytes = totalDowloaded))
 
 
-4.2 unindo dois programa completo para formar um só programa
+####4.2 unindo dois programa completo para formar um só programa
 
 # #coding: utf-8
 # import io
@@ -660,7 +673,7 @@ def dowload(response, output):
 #   if content_length:
 #     length = int(content_length)
 
-4.3 definindo funções main
+####4.3 definindo funções main
 
 def main():
   print("ola")
@@ -672,17 +685,38 @@ if __name__ == "__main__":
 
 
 
-#capitulo 5:
+#capitulo 5: estrutura de dados
 
-# entidades = {
-#     'instituicao' : []
-# }
+####5.1 Montando um modelo conceitual usando estruturas de dados
 
-# # entidades = dict(instituicao = [])
+#####arquivo metadados
 
-entidades = dict()
-entidades['empreendimento'] = 'EntidadeEmpreendimento'
-print(entidades)
+# IdInstituicao bigint Identificador da instituição-PK.
+# IdTipoInstituição bigint identificador do tipo de instituição ...
+# NomInstituição varchar Nome da instituição.
+# NumCnpj varchar Número do CNPJ.
+
+#####instituicao.csv
+
+# 1;1 "Caixa Economica Federal" : "00360305000104"
+# 2;1; "BNDES" : "33657248000189"
+# 8;3; "Governo do estado de minas gerais" : "96313723000117"
+# 12;5; "Governo do distrito federal" : "00394692000108"
+# 105;6; "Concessionário" ; "72036339000159"
+# 106;1; "BNB" ; "07237373000120"
+# 107;2; "INFRAERO" ; "00352294000110"
+
+####5.2 Dicionários: fundação da linguagem
+
+entidades1 = {
+    'Instituicao' : []
+}
+
+entidades1 = dict(instituicao1 = [])
+
+entidades2 = dict()
+entidades2['empreendimento'] = 'EntidadeEmpreendimento'
+print(entidades2)
 
 # del entidades['empreendimento']
 # print(entidades["empreendimento"])
@@ -691,14 +725,59 @@ print(entidades)
 
 # KeyError: 'empreendimento'
 
-entidades = {
-    'instituicao' :[
-        ('IdInstituicao', 'bigint',
-         'Identicador da instituicao-PK'),
-        ('Id tipo de Instituicao')
+entidades3 = {
+    'Instituicao' :[
+        ('IdInstituicao', 'Bigint',
+         'Identicador da Instituicao-PK'),
+        ('Id tipo de Instituicao'),
+        ('NomInstituicao','Varchar','Nome da instituicao'),
+        ('NumCnpj','varchar','Numero do CNPJ')
     ]
 }
 
+##5.3 Montando o dicionário de metadados
+
+import os
+
+for meta_files in os.listdir('data/meta-data'):
+  print(meta_files)
+
+
+####Criando os arquivos de metadados
+
+####Vamos criar os arquivos `empreendimento.txt`, `execucaoFinanceira.txt`, `instituicao.txt` e `licitação.txt` dentro do diretório `data/meta-data` para que `os.listdir` possa encontrá-los.
+
 # import os
-# for meta_files in os.listdir('data/meta-data'):
-#   print(meta_files)
+
+# directory_path = 'data/meta-data'
+
+# # Lista de nomes de arquivos a serem criados
+# file_names = ['Empreendimento.txt', 'execucaoFinanceira.txt', 'instituicao.txt', 'licitacao.txt'] # 'licitação.txt' com 'c' em vez de 'ç' para evitar problemas de codificação se não for tratado
+
+# for file_name in file_names:
+#     file_path = os.path.join(directory_path, file_name)
+#     try:
+#         with open(file_path, 'w') as f:
+#             f.write(f'Conteúdo de teste para {file_name}\n')
+#         print(f"Arquivo '{file_name}' criado com sucesso.")
+#     except Exception as e:
+#         print(f"Erro ao criar o arquivo '{file_name}': {e}")
+
+import os
+
+directory_path = 'data/meta-data'
+
+# files_names
+
+### Verificando novamente os arquivos no diretório
+
+Agora que os arquivos foram criados, vamos listar o conteúdo do diretório `data/meta-data` novamente para confirmar que eles aparecem.
+
+import os
+
+for meta_files in os.listdir('data/meta-data'):
+  print(meta_files)
+
+##5.4 Adicionando e removendo elementos em uma lista
+
+
